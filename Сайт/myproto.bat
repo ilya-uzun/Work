9f3f9f139cc7dc80fed5cd157a8f14af6fcbc58d
ilya-uzun <ilya.uzun@bk.ru>
2021-03-25 22:45:24 +0500

    удалил лишнее

--- Сайт/myproto.bat
+++
@@ -1,9 +0,0 @@
-@echo off
-set "path=%~1"
-setlocal enabledelayedexpansion
-set path=%path:myproto://=%
-set path=%path:"=%
-set path=%path:/=\%
-set path=!path:%%20= !
-set path=!path:%%5C=\!
-C:\Windows\explorer.exe "%path%"
